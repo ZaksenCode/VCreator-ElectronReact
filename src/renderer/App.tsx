06a270640546420cbe065610c9f32e8e41003291
Zaksen <maksimsidorkin2006@mail.ru>
2024-01-20 20:09:56 +0200

    add fonts

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -1,5 +1,6 @@
 import { MemoryRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.scss';
+import './fonts.css';
 import Layout from './components/layout/Layout';
 
 export default function App() {
